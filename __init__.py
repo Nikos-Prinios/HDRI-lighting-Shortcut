08e2af2d697592dc7f691dfde434fd02fed515ef
Nikos <nikos@habanita.org>
2015-12-30 22:53:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-#  Copyright © 2015 Nicolas Priniotakis (Nikos) - nikos@easy-logging.net
+#  2015 Nicolas Priniotakis (Nikos) - nikos@easy-logging.net
 #
 #  This work is free. You can redistribute it and/or modify it under the
 #  terms of the Do What The Fuck You Want To Public License, Version 2,
