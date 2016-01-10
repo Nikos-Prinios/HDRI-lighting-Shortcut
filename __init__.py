91df33786fa3c1a49dca6a6aec6180ce99975ac7
Nikos <Nikos@Colonel.local>
2016-01-10 08:56:57 +0100

    different node setup for HDR and JPG

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
 
 import bpy, pickle, getpass, os
 
-global nodes, node_math, node_map, folder_path, pref, img_path
+global nodes, node_math, node_map, folder_path, pref, img_path, real_HDR, node_bkgnd
 
 pref = os.path.expanduser('~/%s' % 'hdri_prefs')
 if not os.path.exists(pref):
@@ -65,10 +65,9 @@ def node_tree_ok():
 
     current_world = bpy.context.scene.world
     if current_world.name == "HDRI Lighting Shortcut":
-        if node_exists("HLS_ENV"):
-            if node_exists("HLS_MATH"):
-                if node_exists("HLS_MAPPING"):
-                    return True
+        if node_exists("HLS_ENV"): 
+            if node_exists("HLS_MAPPING"):
+                return True
     return False
 
 
@@ -86,10 +85,14 @@ def update_orientation(self, context):
         pass
     
 def update_strength(self, context):
-    try :
+    global real_HDR
+    #try :
+    if not real_HDR:
         node_math.inputs[1].default_value = self.light_strength
-    except :
-        pass
+    else:
+        node_bkgnd.inputs[1].default_value = self.light_strength
+    #except :
+    #    pass
     
 def update_visible(self, context):
     cam = bpy.context.scene.world.cycles_visibility
@@ -103,7 +106,7 @@ def reset():
     try:
         bpy.context.scene.visible = False
         bpy.context.scene.world.cycles_visibility.camera = False
-        bpy.context.scene.light_strength = 0.5
+        bpy.context.scene.light_strength = 1.0
         bpy.context.scene.z_orientation = 0.0
     except:
         pass
@@ -128,7 +131,7 @@ def world_num(world_name):
             return index
 
 def setup(img_path):
-    global nodes,node_math, node_map
+    global nodes,node_math, node_bkgnd, node_map,real_HDR
     
     bpy.context.area.type = 'NODE_EDITOR'
     bpy.context.scene.render.engine = 'CYCLES'
@@ -150,6 +153,9 @@ def setup(img_path):
     nodes = nw_world.node_tree.nodes
     tree = nw_world.node_tree
     img = os.path.basename(img_path)
+    real_HDR = False
+    if img.endswith(".hdr") or img.endswith(".exr"):
+        real_HDR = True
     
     try:
         if not img_exists(img):
@@ -174,10 +180,11 @@ def setup(img_path):
     node_env.image = img
     node_env.location = 200,0
 
-    node_math = nodes.new('ShaderNodeMath')
-    node_math.name = "HLS_MATH"
-    node_math.location = 400,-100
-    node_math.operation = 'ADD'
+    if not real_HDR:
+        node_math = nodes.new('ShaderNodeMath')
+        node_math.name = "HLS_MATH"
+        node_math.location = 400,-100
+        node_math.operation = 'ADD'
 
     node_bkgnd = nodes.new('ShaderNodeBackground')
     node_bkgnd.location = 600,0
@@ -190,8 +197,9 @@ def setup(img_path):
     link0 = links.new(node_coo.outputs[0],node_map.inputs[0])
     link1 = links.new(node_map.outputs[0],node_env.inputs[0])
     link2 = links.new(node_env.outputs[0],node_bkgnd.inputs[0])
-    link3 = links.new(node_env.outputs[0],node_math.inputs[0])
-    link4 = links.new(node_math.outputs[0],node_bkgnd.inputs[1])
+    if not real_HDR:
+        link3 = links.new(node_env.outputs[0],node_math.inputs[0])
+        link4 = links.new(node_math.outputs[0],node_bkgnd.inputs[1])
     link5 = links.new(node_bkgnd.outputs[0],node_out.inputs[0])
 
     bpy.context.scene.world.cycles.sample_as_light = True
@@ -203,7 +211,7 @@ def setup(img_path):
 
 # ----------------- Custom Prop --------------------
 bpy.types.Scene.z_orientation = bpy.props.FloatProperty(name="Orientation",update=update_orientation, max = 360, min = 0, default = 0)
-bpy.types.Scene.light_strength = bpy.props.FloatProperty(name="Strength",update=update_strength, default = 0.5)
+bpy.types.Scene.light_strength = bpy.props.FloatProperty(name="Strength",update=update_strength, default = 1)
 bpy.types.Scene.filepath = bpy.props.StringProperty(subtype='FILE_PATH')  
 bpy.types.Scene.visible = bpy.props.BoolProperty(update=update_visible, name="Visible",description="Let the background being visible by the camera",default = True)
 
