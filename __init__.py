7373f5360e16d17a33de0f5b7886ca99d039cbae
Nikos <Nikos@Colonel.local>
2016-01-21 19:52:15 +0100

    version

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@
 bl_info = {
     "name": "HDRI lighting Shortcut",
     "author": "Nicolas Priniotakis (Nikos)",
-    "version": (1,1,0,0),
+    "version": (1,2,0,0),
     "blender": (2, 7, 6, 0),
     "api": 44539,
     "category": "Material",
